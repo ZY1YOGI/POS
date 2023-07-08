58cbfe313d0c73600d52a8126d3a962d798e2455
ZY1YOGI <zy1yogi@gmail.com>
2023-07-08 04:40:53 +0300

    v0.9

---
+++ components/sidebar/sidebar.tsx
@@ -0,0 +1,86 @@
+'use client';
+import { 
+  useE
+ } from 'react'
+import { AiOutlineHome } from 'react-icons/ai'
+import { BiCategory } from 'react-icons/bi'
+import { MdOutlineNotificationsActive } from 'react-icons/md'
+import { SlBasket } from 'react-icons/sl'
+import { HiOutlineUsers } from 'react-icons/hi'
+import { TbSettingsCog } from 'react-icons/tb'
+import { BsSun } from 'react-icons/bs'
+import { LiaFileInvoiceSolid } from 'react-icons/lia'
+
+import style from './sidebar.module.css'
+
+
+export default function sidebar() {
+  
+  const theme = localStorage.getItem("theme") ?? localStorage.setItem("theme", "light");
+
+  if (theme === "light") {
+    document.documentElement.className = "light";
+    document.documentElement.setAttribute("data-theme", "light");
+  } 
+  else if (theme === "dark") {
+    document.documentElement.className = "dark";
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
+
+  const changeTheme = () => {
+    localStorage.getItem('theme') === 'light' ?
+      (document.documentElement.className = 'dark', document.documentElement.setAttribute('data-theme', 'dark'), localStorage.setItem('theme', 'dark'))
+      : (document.documentElement.className = 'light', document.documentElement.setAttribute('data-theme', 'light'), localStorage.setItem('theme', 'light'))
+  }
+
+
+  return (
+    <aside className="fixed top-0 z-10 h-screen border-r border-gray-700 shadow-lg">
+      <div className="flex flex-col items-center w-16 py-8 space-y-8 backdrop-blur-lg drop-shadow-lg">
+        <a href="#">
+          <img className="w-12 h-12 rounded-full" src="https://t4.ftcdn.net/jpg/04/50/97/71/360_F_450977120_iNKwznk27ghqhL58rvuh3w1xj1mPVHdy.jpg" alt="" />
+        </a>
+
+        <a href="#" className={style.Link + " " + style.LinkActive}>
+          <AiOutlineHome size={35} />
+        </a>
+
+        <a href="/category" className={style.Link}>
+          <BiCategory size={35} />
+        </a>
+
+        <a href="#" className={style.Link}>
+          <HiOutlineUsers size={35} />
+        </a>
+
+        <a href="#" className={style.Link}>
+          <SlBasket size={35} />
+        </a>
+
+        <a href="#" className={style.Link}>
+          <LiaFileInvoiceSolid size={35} />
+        </a>
+
+        <a href="#" className={style.Link}>
+          <MdOutlineNotificationsActive size={35} />
+        </a>
+
+        <a href="#" className={style.Link}>
+          <TbSettingsCog size={35} />
+        </a>
+
+        <a href="#" className={style.Link}>
+          <TbSettingsCog size={35} />
+        </a>
+
+        <a href="#" className={style.Link}>
+          <TbSettingsCog size={35} />
+        </a>
+
+        <button className={style.Link} onClick={changeTheme} role="switch" id="switch-theme">
+          <BsSun size={35} />
+        </button>
+      </div>
+    </aside >
+  )
+}
