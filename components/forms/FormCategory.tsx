a39f519ecfddb1f8d8f44a9db72faa97d7ad08cb
ZY1YOGI <zy1yogi@gmail.com>
2023-08-07 14:50:41 +0300

    Create Category Form Api

--- components/forms/FormCategory.tsx
+++ components/forms/FormCategory.tsx
@@ -16,13 +16,13 @@ export default function FormCategory() {
     const response = await axios.post('/api/category', {
       name: data.name
     })
-    console.log(response)
+    console.log(response.data)
 
 
     // setError('name', {
     //   message: 'The Field is exits'
     // })
-    
+
   }
 
 
@@ -31,7 +31,7 @@ export default function FormCategory() {
     <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-2 mt-6 gap-x-5 gap-y-8 max-md:grid-cols-1">
       <div className='space-y-3'>
         <label htmlFor="input-name" className="x-label">Enter The Name Category <span className='requeued'>*</span></label>
-        <input  {...register("name", { required: "The Name Category is Requeued" })} id="input-name" className="x-input" placeholder="Name" />
+        <input {...register("name", { required: "The Name Category is Requeued" })} id="input-name" className="x-input" placeholder="Name" />
         <span className="block text-red-500">{errors.name && (errors.name.message)}</span>
       </div>
       <div className='flex items-center justify-center'>
